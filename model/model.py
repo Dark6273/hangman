b4cb94d2052960e1342cb492372ac67d8ab9f74f
Dark6273 <mahdi.khosravi6273@gmail.com>
2022-03-08 13:32:59 +0330

    update Source and add comment

--- model/model.py
+++ model/model.py
@@ -1,9 +1,10 @@
+# Import needed library
 import view.banner as banner
 from sys import exit
 from time import sleep
 from random import choice
 
-
+# Define variable
 USERNAME = ''
 WORD = ''
 FOUND = ''
@@ -11,23 +12,25 @@ LIVE = 6
 NOTIN = ''
 
 
+# Get username and check in score file
 def get_user_name():
     global USERNAME
-    USERNAME = banner.get_user_name()
+    USERNAME = banner.get_user_name() # Get username
     
-    with open('data/score') as f:
+    with open('data/score') as f: # Read data from score file
         users = f.readlines()
-    users = [user.strip() for user in users]
-    users = [user.split(',')[0] for user in users]
+    users = [user.strip() for user in users] # Remove \n
+    users = [user.split(',')[0] for user in users] # split as , 
     
-    if USERNAME not in users:
-        file = open('data/score', 'a+')
-        file.write(USERNAME+',0,0\n')
-        file.close()
-        banner.show('The user successfully added', USERNAME, '@')
-        sleep(1.5)
+    if USERNAME not in users: # If the user did not exist
+        file = open('data/score', 'a+') # Open file as a+
+        file.write(USERNAME+',0,0\n') # Write a new user
+        file.close() # Close file
+        banner.show('The user successfully added', USERNAME, '@') # Show banner add user
+        sleep(1) # Sleep 1s 
 
 
+# Reset global variables
 def reset():
     global WORD, FOUND, LIVE
     WORD = ''
@@ -35,14 +38,16 @@ def reset():
     LIVE = 6
 
 
+# Write new data in score fine
 def write(users):
-    file = open('data/score', 'w')
+    file = open('data/score', 'w') # Open file as w
     for user in users:
         text = user[0] + ',' + str(user[1]) + ',' + str(user[2]) + '\n'
-        file.write(text)
-    reset()
+        file.write(text) # Wrinte in File
+    reset() # Call reser function
     
 
+# Update score user
 def win():
     user = ['', '', '']
     with open('data/score') as f:
@@ -57,6 +62,7 @@ def win():
     write(users)
 
 
+# Update score user
 def lose():
     user = ['', '', '']
     with open('data/score') as f:
@@ -71,10 +77,12 @@ def lose():
     write(users)
 
 
+# Check character if exist in word or not exist
 def get_char(char):
     global FOUND, LIVE, NOTIN
+    
     while LIVE > 0 and WORD != FOUND:
-        if char in WORD:
+        if char in WORD: # If exist in word 
             count = 0
             ls = list(FOUND)
             text = ''
@@ -85,7 +93,7 @@ def get_char(char):
             for i in ls:
                 text += i
             FOUND = text
-        else:
+        else: # If not exist
             LIVE -= 1
             
         if len(NOTIN) > 0:
@@ -95,94 +103,99 @@ def get_char(char):
         if LIVE > 0 and WORD != FOUND:
             get_char(banner.get_char(FOUND, USERNAME, LIVE, NOTIN))
     
-    if LIVE > 0:
-        banner.word(WORD, LIVE, NOTIN)
-        banner.show('good job. you win', USERNAME, ':)')
-        sleep(1)
-        win()
-        main()
-    else:
-        banner.word(WORD, LIVE, NOTIN)
-        banner.show('Unfortunately you lost', USERNAME, ':(')
-        sleep(1)
-        lose()
-        main()
+    if LIVE > 0: # If win user
+        banner.word(WORD, LIVE, NOTIN) # Show word banner
+        banner.show('good job. you win', USERNAME, ':)') # Print win text
+        sleep(1) # Sleep 1s
+        win() # Call win function
+        main() # Call main function
+    else:  # If lose user
+        banner.word(WORD, LIVE, NOTIN) # Show word banner
+        banner.show('Unfortunately you lost', USERNAME, ':(') # Print lose text
+        sleep(1) # Sleep 1s
+        lose() # Call lose function
+        main() # Call main funciton
         
 
+# Start game and Choice word
 def start_game():
     global WORD,FOUND
-    with open('data/words') as f:
+    with open('data/words') as f: # Read words in file words
         words = f.readlines()
-    words = [w.strip() for w in words]
-    try:
+    words = [w.strip() for w in words] # Remove \n
+    try: # Try get int level
         banner.level()
         level = int(banner.select('', USERNAME, '?'))
     except:
         level = -1
         
-    if level == 1:
+    if level == 1: # len word < 5
         word = choice(words)
         while len(word) > 5:
             word = choice(words)
-    elif level == 2:
+    elif level == 2: # len word < 10
         word = choice(words)
         while len(word) > 10:
             word = choice(words)
-    elif level == 3:
+    elif level == 3: # len word > 10
         word = choice(words)
         while len(word) < 5:
             word = choice(words)
-    else:
+    else: # Wrong input
         wrong()
     
     WORD = word
     for i in word:
         FOUND += '-'
     
-    banner.show('The game has been successfully created', USERNAME, '!')    
-    sleep(1)
-    get_char(banner.get_char(FOUND, USERNAME, LIVE, NOTIN))
+    banner.show('The game has been successfully created', USERNAME, '!')# Start game and
+    sleep(1) # Sleep 1s
+    get_char(banner.get_char(FOUND, USERNAME, LIVE, NOTIN)) # Call function from banner file
 
 
+# Get score data
 def score():
-    with open('data/score') as f:
+    with open('data/score') as f: # Open file score and read lines
         users = f.readlines()
-    users = [user.strip() for user in users]
-    users = [user.split(',') for user in users]
+    users = [user.strip() for user in users] # Remove \n
+    users = [user.split(',') for user in users] # Split ,
     
-    for user in users:
+    for user in users: # Find user
         if USERNAME == user[0]:
             users = user
             pass
         
-    banner.show('The user has >> {} wins << and >> {} lost <<'.format(users[1], users[2]), USERNAME, '#')
-    sleep(3)
-    main()
+    banner.show('The user has >> {} wins << and >> {} lost <<'.format(users[1], users[2]), USERNAME, '#') # Call score banner
+    sleep(3) # Sleep 3s
+    main() # Call main function
 
 
+# bye function for exit game
 def bye():
-    banner.bye()
-    sleep(1)
-    exit()
+    banner.bye() # Show bye banner
+    sleep(1) # Sleep 1s
+    exit() # Exit from app
 
 
+# Wrong banner if user select false option
 def wrong():
-    banner.wrong()
-    sleep(0.7)
-    main()
+    banner.wrong() # Call wrong banner
+    sleep(0.7) # Sleep 0.7s
+    main() # Call main
     
 
+# Main function
 def main():
-    try:
+    try: # Try get int from user
         select = int(banner.main_page(USERNAME))
     except:
         select = -1
     
-    if select == 1:
+    if select == 1: # user select 1 start game
         start_game()
-    elif select == 2:
+    elif select == 2: # user select 2 show score
         score()
-    elif select == 0:
+    elif select == 0: # user select 0 exit app
         bye()
-    else:
+    else: # user false select
         wrong()
--- view/banner.py
+++ view/banner.py
@@ -1,3 +1,4 @@
+# Import needed library
 from colorama import Fore
 from pyfiglet import Figlet
 from os import system, name
@@ -7,6 +8,7 @@ from time import sleep
 f = Figlet(font='big')
 
 
+# Main logo
 def main_banner():
     system('cls' if name == 'nt' else 'clear')
     print(Fore.CYAN + """
@@ -23,6 +25,7 @@ def main_banner():
           """)
 
     
+# Print menu
 def menu():
     sleep(0.05)
     print(Fore.RED + ' [' + Fore.WHITE + '#' + Fore.RED + ']' + Fore.BLUE + " Choose one of the options below\n")
@@ -33,7 +36,8 @@ def menu():
     sleep(0.05)
     print(Fore.RED + ' [0] Exit...' + Fore.RESET)
     
-    
+
+# Print menu level
 def level():
     sleep(0.05)
     print(Fore.RED + ' [' + Fore.WHITE + '#' + Fore.RED + ']' + Fore.BLUE + " Choose your desired game level\n")
@@ -45,13 +49,14 @@ def level():
     print(Fore.RED + ' [3] Hard')
 
 
-def word(word, live, notin):
-    sleep(0.3)
-    system('cls' if name == 'nt' else 'clear')
-    print(Fore.RED + f.renderText(str(word)))
-    print(Fore.YELLOW + " The selected word has {} letters".format(len(word)))
-    print(Fore.YELLOW + " The letters used so far " + Fore.WHITE + notin)
-    print(Fore.YELLOW + " Remaining chance : {}".format('❤️' * live))
+# Print Word and Input for get charcater
+def word(word, live, notin): 
+    sleep(0.3) # sleep 0.3 s
+    system('cls' if name == 'nt' else 'clear') # Clear screen
+    print(Fore.RED + f.renderText(str(word))) # Print character founded user
+    print(Fore.YELLOW + " The selected word has {} letters".format(len(word))) # Len word
+    print(Fore.YELLOW + " The letters used so far " + Fore.WHITE + notin) # Used character
+    print(Fore.YELLOW + " Remaining chance : {}".format('❤️' * live)) # Count of live
 
 
 # Select user   
@@ -65,9 +70,10 @@ def select(text, title, logo):
         return -1
     
  
+ # Show box for get character and word
 def get_char(found, username, live, notin):
-    word(found, live, notin)
-    return select("Enter the character you want ", username, ':)')
+    word(found, live, notin) # Print word
+    return select("Enter the character you want ", username, ':)') # Input get charcter
  
     
 # Select user   
@@ -76,9 +82,10 @@ def show(text, title, logo):
     return print(Fore.RED + "\n ┌─[" + Fore.GREEN + ">> " + Fore.WHITE + "HANGMAN "+ Fore.GREEN + "~ " + Fore.BLUE + title + Fore.GREEN +" <<" + Fore.RED + "]\n └──╼ [" + Fore.WHITE + logo + Fore.RED +"] "+ Fore.YELLOW + text + Fore.RESET + " ")
 
 
+# Show page for get username
 def get_user_name():
-    main_banner()
-    return select('Enter your username ', 'USERNAME', '$')
+    main_banner() # Main banner
+    return select('Enter your username ', 'USERNAME', '$') # Get username from user
 
     
 # Print Good Bye :)
@@ -91,7 +98,8 @@ def wrong():
     print(Fore.RED + "\n ┌─[" + Fore.GREEN + ">> " + Fore.WHITE + "TIMER "+ Fore.GREEN +"<<" + Fore.RED + "]\n └──╼ [" + Fore.WHITE + "?" + Fore.RED +"] " + Fore.YELLOW + 'The input structure has not been observed !!')
 
 
+# Print Main Menu 
 def main_page(username):
-    main_banner()
-    menu()
-    return select('', username, '#')
+    main_banner() # Main banner
+    menu() # Print menu
+    return select('', username, '#') # Get input from user
